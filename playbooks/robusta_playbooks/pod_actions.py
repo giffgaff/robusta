f325fb34464b3d8def1c568f655e2e934fd5adf3
12ushan <rushan@giffgaff.co.uk>
2022-07-07 12:15:25 +0100

    [SRE-1310] test slack notif with pod delete - robusta

--- playbooks/robusta_playbooks/pod_actions.py
+++ playbooks/robusta_playbooks/pod_actions.py
@@ -8,6 +8,25 @@ def delete_pod(event: PodEvent):
     """
     if not event.get_pod():
         logging.info("Failed to get the pod for deletion")
+        event.add_enrichment(
+            [
+                MarkdownBlock(
+                    "*Alert Explanation:* Robusta FAILED to delete faulty POD, please action manually."
+
+                ),
+            ],
+            annotations={SlackAnnotations.UNFURL: False},
+        )
         return
 
     event.get_pod().delete()
+
+    event.add_enrichment(
+        [
+            MarkdownBlock(
+                "*Alert Explanation:* The faulty pod has now been deleted by Robusta"
+
+            ),
+        ],
+        annotations={SlackAnnotations.UNFURL: False},
+    )
